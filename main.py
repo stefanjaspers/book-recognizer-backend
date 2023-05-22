1d39cb53bb4970f27581fe9e2c269792b47bed72
stefanjaspers <s.jaspers1997@gmail.com>
2023-05-22 17:40:53 +0200

    Print statements on FastAPI events.

--- main.py
+++ main.py
@@ -14,13 +14,17 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup_mongo_client():
+    print("Instantiating MongoDB connection...")
     app.mongodb_client = AsyncIOMotorClient(mongo_config.MONGO_URI)
     app.mongodb = app.mongodb_client[mongo_config.MONGO_DB_NAME]
+    print("MongoDB connection established successfully.")
 
 
 @app.on_event("shutdown")
 async def shutdown_mongo_client():
+    print("Closing MongoDB connection...")
     app.mongodb_client.close()
+    print("MongoDB connection closed successfully.")
 
 
 app.include_router(auth.router)
